85bc597e16bdccfbbd02ad5e385a195ee1f09369
Stephen Center <stephenbcenter@gmail.com>
2018-11-26 10:58:06 -0500

    Did more stuff

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data {
+    public class Note {
+        [Key]
+        public int NoteID { get; set; }
+
+        [Required]
+        public Guid OwnerID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUTC { get; set; }
+
+        public DateTimeOffset? ModifiedUTC { get; set; }
+    }
+}
